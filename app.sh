3fececa1293d5239dc840ce16c684cc79a6809e2
James Bartlett <jimmyb@gmail.com>
2020-05-30 12:55:59 -0500

    fixed workbench launch script

--- app.sh
+++ app.sh
@@ -3,4 +3,3 @@ docker run --rm -d -p 127.0.0.1:3004:3004 \
     -v /dev/bus/usb:/dev/bus/usb \
     --device-cgroup-rule='c 189:* rmw' \
     jamesdbartlett3/intel-p1-people-counter
-echo "finished"
--- docker/dl_workbench.sh
+++ docker/dl_workbench.sh
@@ -1,6 +1,5 @@
-#/bin/bash
-docker run -p 127.0.0.1:5665:5665 \
+docker run --rm -p 127.0.0.1:5665:5665 \
         -v /dev/bus/usb:/dev/bus/usb \
         --device-cgroup-rule='c 189:* rmw' \
-        -volume ~/.workbench:/home/openvino/.workbench \
-        -it openvino/workbench:latest
+        -it openvino/workbench:latest \
+        -volume ~/.workbench:/home/openvino/.workbench
